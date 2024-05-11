c89e80b2b891173bab97480e902dd6aa892f5098
DenisCunha <ded_2004@hotmail.com>
2024-05-11 13:27:05 -0300

    inserindo codigo sugerido pelo chat GPT

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,10 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+# Receber os dados do usuário
+dados1 = input("Digite o primeiro dado: ")
+dados2 = input("Digite o segundo dado: ")
+
+# Concatenar os dados em uma única string
+concatenado = dados1 + dados2
+
+# Mostrar o resultado
+print("Os dados concatenados são: ", concatenado)
