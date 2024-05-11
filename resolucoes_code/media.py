0c5675e35ed0f01e333eaf3c7b2ec820cb73221d
DenisCunha <ded_2004@hotmail.com>
2024-05-11 14:22:45 -0300

    melhorando o codigo para resposta em 2 casa deciamis  e inserindo uma nova nota4

--- resolucoes_code/media.py
+++ resolucoes_code/media.py
@@ -1,13 +1,14 @@
 # Descrição: Agora vamos calcular a média de três notas fornecidas na entrada do usuário. 
 
-def calcular_media(nota1, nota2, nota3):
-    return (nota1 + nota2 + nota3) / 3
+def calcular_media(nota1, nota2, nota3, nota4):
+    return (nota1 + nota2 + nota3 + nota4) / 4
 
 # Solicitar as três notas como entrada do usuário
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
 
 # Chamar a função calcular_media e exibir o resultado
-media = calcular_media(nota1, nota2, nota3)
-print("A média das três notas é:", media)
+media = calcular_media(nota1, nota2, nota3, nota4)
+print("A média das três notas é:", round( media, 2))
